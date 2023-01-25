36e1f09fcecd41424a5af73a14d7bf0437eea172
shenxrr <x5shen@ucsd.edu>
2023-01-25 15:52:28 -0800

    debug

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test 
+	public void testReverseInPlace1() {
+    int[] input2 = { 1,2,3 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{ 3,2,1 }, input2);
+	}
+
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+ @Test
+ public void testReversed1() {
+  int[] input2 = { 1,2,3 };
+  assertArrayEquals(new int[] {3,2,1 }, ArrayExamples.reversed(input2));
+ }
 }
